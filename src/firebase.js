7a48016fe1a1c4ab8f9b1da182a6fc9166fc6b67
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-21 13:40:28 -0600

    firebase file -credentials to access database

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/database";
+
+const config = {
+  apiKey: "AIzaSyDhVCbO1Dhi_IXeyPI0be2bhlbx2cvQE6I",
+  authDomain: "m-city-bb139.firebaseapp.com",
+  databaseURL: "https://m-city-bb139.firebaseio.com",
+  projectId: "m-city-bb139",
+  storageBucket: "m-city-bb139.appspot.com",
+  messagingSenderId: "303912881913"
+};
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref("matches");
+
+export { firebase, firebaseMatches };
