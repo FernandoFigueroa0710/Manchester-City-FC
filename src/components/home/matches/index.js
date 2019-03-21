4d0f5b8e265bad9abbd2dea06b4ab7786d0d6a42
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-21 13:39:01 -0600

    create Blocks component
    Logic to handle data from the firebase database done

--- src/components/home/matches/index.js
+++ src/components/home/matches/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Tag } from "../../Utils/misc";
+import Blocks from "../matches/Blocks";
 
 const MatchesHome = () => {
   return (
@@ -8,7 +9,7 @@ const MatchesHome = () => {
         <Tag bck="#0e1731" size="50px" color="#ffffff">
           Matches
         </Tag>
-        BLOCK
+        <Blocks />
         <Tag
           bck="#ffffff"
           size="22px"
