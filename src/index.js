cd05c0de9acac467c79ddc1b744531709473bce4
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-21 13:37:28 -0600

    update index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Resources/css/app.css";
 import { BrowserRouter } from "react-router-dom";
-
 import Routes from "./containers/Routes";
 
 const App = () => {
