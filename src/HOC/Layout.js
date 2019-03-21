5e651e41f9b26c6fe2cb30af29c372d082eb86b5
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-20 19:52:48 -0600

    footer set up

--- src/HOC/Layout.js
+++ src/HOC/Layout.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Header from "../components/header_footer/Header";
+import Footer from "../components/header_footer/Footer";
+
 const Layout = props => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
