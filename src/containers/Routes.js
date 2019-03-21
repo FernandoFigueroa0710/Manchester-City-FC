f0a04d07a352005264420473b68febb4a3471f35
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-21 00:21:04 -0600

    set up the home route

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Layout from "../HOC/Layout";
 
+import { Switch, Route } from "react-router-dom";
+
+import Home from "../components/home";
+
 const Routes = props => {
-  return <Layout>Hellloooooo</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
+  );
 };
 
 export default Routes;
