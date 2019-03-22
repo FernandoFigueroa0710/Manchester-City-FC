025e9474deb5803eb315349868df6096ab19cd81
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-21 18:24:00 -0600

    card component set up

---
+++ src/components/home/meet_players/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Stripes from "../../../Resources/images/stripes.png";
+import { Tag } from "../../Utils/misc";
+
+class MeetPlayers extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div
+        className="home_meetplayers"
+        style={{
+          background: `#ffffff url(${Stripes})`
+        }}
+      >
+        <div className="container">
+          <div className="home_meetplayers_wrapper">
+            <div className="home_card_wrapper">CARD Component</div>
+            <div className="home_text_wrapper">TEXT COMPONENT</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MeetPlayers;
