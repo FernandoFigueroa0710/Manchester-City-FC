8f2d153db4573e43ca66a7d256a99d6c61e2c605
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-23 18:48:39 -0600

    Enroll  and form fiel set up
    acepptin value as the input text on email field
    
    Still missing validation form for email

--- src/components/home/promotion/Enroll.js
+++ src/components/home/promotion/Enroll.js
@@ -1,1 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+import FormField from "../../Utils/FormFields";
+
+class Enroll extends Component {
+  state = {
+    formError: false,
+    formSucess: "",
+    formdata: {
+      email: {
+        element: "input",
+        value: "",
+        config: {
+          name: "email_input",
+          type: "email",
+          placeholder: "Enter Your Email"
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        validationMessage: ""
+      }
+    }
+  };
+  updateForm(element) {
+    const newFormdata = { ...this.state.formdata };
+    const newElement = { ...newFormdata[element.id] };
+
+    newElement.value = element.event.target.value;
+    newFormdata[element.id] = newElement;
+
+    this.setState({
+      formdata: newFormdata
+    });
+  }
+  submitForm() {}
+
+  render() {
+    return (
+      <Fade>
+        <div className="enroll_wrapper">
+          <form onSubmit={event => this.submitForm(event)}>
+            <div className="enroll_title">Enter your email</div>
+            <div className="enroll_input">
+              <FormField
+                id={"email"}
+                formdata={this.state.formdata.email}
+                change={element => this.updateForm(element)}
+              />
+            </div>
+          </form>
+        </div>
+      </Fade>
+    );
+  }
+}
+
+export default Enroll;
