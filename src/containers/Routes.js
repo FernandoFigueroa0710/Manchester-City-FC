783a61cbdf40d17df4f579c72c9b2dae9f600edb
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-24 14:33:00 -0600

    public routes set up
    Public routes done, able to verify and identify user

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../HOC/Layout";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 import Home from "../components/home";
 import SignIn from "../components/signin";
 import PrivateRoute from "../components/authRoutes/PrivateRoutes";
+import PublicRoute from "../components/authRoutes/PublicRoutes";
 
 import Dashborad from "../components/admin/Dashboard";
 
@@ -18,8 +19,20 @@ const Routes = props => {
           extact
           component={Dashborad}
         />
-        <Route exact component={SignIn} path="/sign_in" />
-        <Route exact component={Home} path="/" />
+        <PublicRoute
+          {...props}
+          restricted={true}
+          exact
+          component={SignIn}
+          path="/sign_in"
+        />
+        <PublicRoute
+          {...props}
+          restricted={false}
+          path="/"
+          extact
+          component={Home}
+        />
       </Switch>
     </Layout>
   );
