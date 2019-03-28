77d04b2e2e7d3e8add145056412be24655ef9acc
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-27 21:34:16 -0600

    add function for teams data
    export the DB

--- src/firebase.js
+++ src/firebase.js
@@ -16,4 +16,12 @@ firebase.initializeApp(config);
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref("matches");
 const firebasePromotions = firebaseDB.ref("promotions");
-export { firebase, firebaseMatches, firebasePromotions };
+const firebaseTeams = firebaseDB.ref("teams");
+
+export {
+  firebase,
+  firebaseDB,
+  firebaseMatches,
+  firebasePromotions,
+  firebaseTeams
+};
