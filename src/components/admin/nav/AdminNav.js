906f4ea95a464c0023767a418d31677fff17632a
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-27 23:07:50 -0600

    fixed bug in links

--- src/components/admin/nav/AdminNav.js
+++ src/components/admin/nav/AdminNav.js
@@ -12,7 +12,7 @@ const AdminNav = () => {
     },
     {
       title: "Add Match",
-      linkTo: "/admin_matches/edit_match/:id"
+      linkTo: "/admin_matches/edit_match"
     },
     {
       title: "Players",
