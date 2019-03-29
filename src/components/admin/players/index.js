7e41f9314fb2f490989c08368427e3dcd8849f00
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-28 20:57:45 -0600

    player list generated from DB

--- src/components/admin/players/index.js
+++ src/components/admin/players/index.js
@@ -30,11 +30,50 @@ class AdminPlayers extends Component {
   }
 
   render() {
-    console.log(this.state);
     return (
-      <div>
-        <div>Players</div>
-      </div>
+      <AdminLayout>
+        <div>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Number</TableCell>
+                  <TableCell>Position</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.players
+                  ? this.state.players.map((player, i) => (
+                      <TableRow key={i}>
+                        <TableCell>
+                          <Link to={`/admin_players/add_players/${player.id}`}>
+                            {player.name}
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                          <Link to={`/admin_players/add_players/${player.id}`}>
+                            {player.lastname}
+                          </Link>
+                        </TableCell>
+                        <TableCell>{player.number}</TableCell>
+                        <TableCell>{player.position}</TableCell>
+                      </TableRow>
+                    ))
+                  : null}
+              </TableBody>
+            </Table>
+          </Paper>
+          <div className="admin_progress">
+            {this.state.isLoading ? (
+              <CircularProgress thickness={6} style={{ color: "#98c5e9" }} />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </AdminLayout>
     );
   }
 }
