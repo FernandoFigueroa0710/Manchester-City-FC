8667329aca992b6bf43d964d1f88dc6bbc0534fe
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-30 11:26:08 -0600

    file uploader reusable component

---
+++ src/components/Utils/FileUploader.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FileUploader extends Component {
+  render() {
+    return (
+      <div>
+        <div>File uploader component</div>
+      </div>
+    );
+  }
+}
+
+export default FileUploader;
