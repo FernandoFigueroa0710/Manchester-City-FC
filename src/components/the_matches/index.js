2c31ffc872c6951bc47fcae956ec25259d1f56b3
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-04-05 19:47:17 -0600

    League table component added
    League table rendering data and working properly

--- src/components/the_matches/index.js
+++ src/components/the_matches/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { firebaseMatches } from "../../firebase";
+import { firebaseLooper, reverseArray } from "../Utils/misc";
+import LeagueTable from "./table";
 
 class TheMatches extends Component {
+  state = {
+    isLoading: true,
+    matches: [],
+    filtereMatches: [],
+    playerFilter: "All",
+    resultFilter: "All"
+  };
+
+  componentDidMount() {
+    firebaseMatches.once("value").then(snapshot => {
+      const matches = firebaseLooper(snapshot);
+      this.setState({
+        isLoading: false,
+        matches: reverseArray(matches),
+        filtereMatches: reverseArray(snapshot)
+      });
+    });
+  }
   render() {
+    const state = this.state;
     return (
-      <div>
-        <div>The matches</div>
+      <div className="the_matches_container">
+        <div className="the_matches_wrapper">
+          <div className="left">The matches right</div>
+          <div className="right">
+            <LeagueTable />
+          </div>
+        </div>
       </div>
     );
   }
